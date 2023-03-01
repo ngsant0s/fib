994341c360d48b2fb97756cceb64650152f6ece0
ngsant0s <ngsantos.git@proton.me>
2023-03-01 20:30:50 -0300

    redis inicialization

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,10 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
+RUN apt-get update && \
+    apt-get install -y redis-tools && \
+    rm -rf /var/lib/apt/lists/*
+
 RUN python app_test.py
 
 ENTRYPOINT [ "python", "./app.py" ]
--- app.py
+++ app.py
@@ -1,10 +1,28 @@
+import redis
+
+#Conectar ao cache Redis
+redis_host = 'localhost'
+redis_port = 6379
+redis_db = 0
+redis_conn = redis.Redis(host=redis_host, port=redis_port, db=redis_db)
+
+#Função para calcular o N fibonacci
 def fibonacci(number) -> int:
     '''Fibonacci N digit calculation'''
     if number < 2:
         return number 
     return fibonacci(number - 1) + fibonacci(number - 2)
 
+
+def redis_cache_write():
+    for i in range(1, 11):
+        fib_number = fibonacci(i)
+        redis_conn.set(f"fib_{i}", fib_number)
+
 if __name__ == '__main__':
+
+    #Inicialização dos 10 primeiros digitos no redis cache
+    redis_cache_write()
 
     #Seleção de um digito para calculo
     number = int(input("Qual digito da sequencia de Fibonacci deseja?\n"))
