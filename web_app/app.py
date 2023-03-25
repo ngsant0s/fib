10016ef0ea36ba6c9bc31495d74bd90bb256845b
ngsant0s <ngsantos.git@proton.me>
2023-03-25 00:11:50 -0300

    Fix postgres connection

--- web_app/app.py
+++ web_app/app.py
@@ -5,7 +5,7 @@ import psycopg2
 
 app = Flask(__name__, template_folder='templates')
 app.config['REDIS_URL'] = 'redis://cache:6379/0'
-app.config['POSTGRES_URL'] = 'postgresql://postgres:mypasswd@localhost:5432/fib_db'
+app.config['POSTGRES_URL'] = 'postgresql://postgres:mypasswd@database:5432/fib_data'
 
 redis_cache = Redis(app)
 
@@ -25,14 +25,14 @@ def postgres_connection():
     return psycopg2.connect(app.config['POSTGRES_URL'])
 
 def postgres_write(number, fib_sequence):
-    with get_postgres_connection() as conn:
+    with postgres_connection() as conn:
         with conn.cursor() as cur:
             cur.execute("INSERT INTO fibonacci_numbers (numbers, fib_value) VALUES (%s, %s)", (number, json.dumps(fib_sequence)))
 
 def get_sequence_postgres(number):
-    with get_postgres_connection() as conn:
+    with postgres_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute("SELECT sequence FROM fibonacci_numbers WHERE numbers = %s", (number,))
+            cur.execute("SELECT numbers FROM fibonacci_numbers WHERE numbers = %s", (number,))
             result = cur.fetchone()
             if result:
                 return json.loads(result[0])
@@ -47,7 +47,7 @@ def index():
         if result:
             result = json.loads(result)
         else:
-            result = get_fibonacci_sequence_from_postgres(number)
+            result = get_sequence_postgres(number)
             if not result:
                 result = fibonacci(int(number))
                 data = {"number": number, "fibonacci": result}
@@ -66,7 +66,7 @@ def calculate():
         if result:
             result = json.loads(result)
         else:
-            result = get_fibonacci_sequence_from_postgres(number)
+            result = get_sequence_postgres(number)
             if not result:
 
                 result = fibonacci(int(number))
@@ -79,4 +79,4 @@ def calculate():
     return render_template('index.html')
 
 if __name__ =='__main__':
-    app.run(host='0.0.0.0', port=5000, portdebug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
