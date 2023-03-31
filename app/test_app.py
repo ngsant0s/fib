d179d21aaf583151ea5d605c92b8cac10c79c90c
ngsant0s <ngsantos.git@proton.me>
2023-03-31 14:58:12 -0300

    Added nginx and healthchecks

--- app/test_app.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from app import fibonacci
-
-class TestFibonacci(unittest.TestCase):
-    def test_fibonacci(self):
-        self.assertEqual(fibonacci(15), 610)
-    
-if __name__ == '__main__':
-    unittest.main()
